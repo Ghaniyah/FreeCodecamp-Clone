a10f57c8a69353960e325a187ccf33c5988ffa24
idris lawal <idriseun222@gmail.com>
2023-06-04 18:36:09 +0100

    feat: translation api

---
+++ translation-project/translation.js
@@ -0,0 +1,20 @@
+const key = ''
+
+async function getTranslation(text, language) {
+    let result = await fetch(`https://translation.googleapis.com/language/translate/v2?q=${text}&target=${language}&key=${key}`)
+
+    let translationData = await result.json()
+
+    return translationData.data.translations[0].translatedText
+}
+
+async function getTranslationFromAPI() {
+    console.log('button clicked')
+    let textToTranslate = document.getElementById('translation-text').value
+
+    let language = document.getElementById('select-lang').value
+
+    let translatedText = await getTranslation(textToTranslate, language)
+
+    document.getElementById('trans-result').innerHTML = translatedText
+}
